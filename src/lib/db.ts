b2e45b16f904dd2252a9aabb7a19b38bbf5468a2
Fermin Cirella <letrix@outlook.es>
2021-11-25 17:41:39 -0300

    Fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,4 +4,4 @@ dotenv.config()
 const { MYSQL_URI, POSTGRES_URI, POSTGRES_URI_ELEPHANT } = process.env
 
 import { Sequelize } from 'sequelize';
-export default new Sequelize(POSTGRES_URI_ELEPHANT, {pool: {max: 2}})
+export default new Sequelize('postgres://dswzpyhu:LmLCCjoquc9ADWbIhNNFikRzBfN8mXCG@kesavan.db.elephantsql.com/dswzpyhu', {pool: {max: 2}})
